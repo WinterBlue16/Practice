01dc19d737684d977422578261a585e0a53c1922
Winter Lee <leekh090163@gmail.com>
2020-03-07 21:53:29 +0900

    전처리 중

---
+++ notebook.py
@@ -0,0 +1,15 @@
+# from itertools import product
+# a, b, c = map(int, input().split())
+# def m(x):
+#     X=[]
+#     for i in range(x):
+#         X.append(i)
+#     return X
+# lisum=[m(a), m(b), m(c)]
+# a=list(product(*lisum))
+# for i in range(len(a)):
+#     v=list(a[i])
+#     for i in range(len(v)):
+#         print(v[i], end=' ')
+#     print(' ')
+# print(len(a)) # 84번 문제
