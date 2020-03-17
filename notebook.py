2e01174d3e35d2e9013f9da80aeb620b8f74241d
Winter Lee <leekh090163@gmail.com>
2020-03-17 10:38:44 +0900

    Files revised

--- notebook.py
+++ notebook.py
@@ -13,3 +13,16 @@
 #         print(v[i], end=' ')
 #     print(' ')
 # print(len(a)) # 84번 문제
+
+# M = [[0 for col in range(19)] for row in range(19)]
+# n = int(input())
+# for i in range(n): 
+#     i, j = map(int, input().split())
+#     M[i-1][j-1] = 1 # 인덱스 번호를 좌표 번호와 일치시킨다
+    
+# for i in M:
+#     print(" ".join(map(str, i))) # 리스트의 대괄호와 ,를 동시에 없애준다
+
+print(ord('┌'))
+
+print(chr(9484))
